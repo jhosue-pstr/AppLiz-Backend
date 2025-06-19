2572f254a41affd78ba084f0b77130d6555e3d31
jhosue-pstr <pastor.jhosue07@gmail.com>
2025-06-19 04:36:56 -0500

    agregnado endpointd3

--- src/routes/rewards.py
+++ src/routes/rewards.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from src.models.points import PointSystem, RewardSystem
+from src.models.points import PointSystem
 from src.config.settings import token_required
 
 rewards_bp = Blueprint('rewards', __name__)
@@ -9,24 +9,6 @@ rewards_bp = Blueprint('rewards', __name__)
 def get_balance():
     balance = PointSystem.get_balance(request.user_id)
     return jsonify({"balance": balance}), 200
-
-@rewards_bp.route('/rewards', methods=['GET'])
-@token_required
-def list_rewards():
-    rewards = RewardSystem.get_available_rewards()
-    return jsonify(rewards), 200
-
-@rewards_bp.route('/redeem', methods=['POST'])
-@token_required
-def redeem_reward():
-    data = request.get_json()
-    success = RewardSystem.redeem_reward(
-        request.user_id,
-        data['reward_id']
-    )
-    if success:
-        return jsonify({"message": "Recompensa canjeada"}), 200
-    return jsonify({"error": "Puntos insuficientes o recompensa no disponible"}), 400
 
 
 
